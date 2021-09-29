b5478c6aa084a1bf60d79bdcffe36742504752ae
Benjamin Tarenne <b.tarenne@thoughtworks.com>
2021-09-29 17:38:58 +0200

    Add uuid identifier

--- src/com/thoughtworks/ddd_workshop/application/Main.java
+++ src/com/thoughtworks/ddd_workshop/application/Main.java
@@ -4,24 +4,42 @@ import com.thoughtworks.ddd_workshop.domain.Cart;
 import com.thoughtworks.ddd_workshop.domain.Item;
 import com.thoughtworks.ddd_workshop.domain.Product;
 
+import java.util.UUID;
+
 public class Main {
 
     public static void main(String[] args) {
         Cart cart = new Cart();
+        Cart cart1 = new Cart();
+        Cart cart2 = new Cart();
         Product ipadpro = new Product("IPad Pro");
+        Product ipadpro1 = new Product("IPad Pro");
+        Product ipadpro2 = new Product("IPad Pro");
         Product pen = new Product("Hero ink Pen");
         Product shoes = new Product("Reebok Cricket bat");
         Product foo = new Product("foo");
 
         cart.addItem(new Item(ipadpro, 1));
+        cart1.addItem(new Item(ipadpro1, 1));
+        cart2.addItem(new Item(ipadpro2, 1));
         cart.addItem(new Item(pen, 1));
         cart.addItem(new Item(shoes,2));
 
         cart.removeItem(ipadpro);
         cart.removeItem(foo);
 
+        System.out.println(cart1.equals(cart1));
+        System.out.println(cart1.equals(cart2));
+
+        UUID uuid = UUID.randomUUID();
+        Cart cart3 = new Cart(uuid);
+        Cart cart4 = new Cart(uuid);
+        System.out.println(cart3.equals(cart4));
+
+
         System.out.println(cart.getItems().toString());
         System.out.println("-------------------------");
         System.out.println(cart.getDeletedProducts().toString());
+
     }
 }
--- src/com/thoughtworks/ddd_workshop/domain/Cart.java
+++ src/com/thoughtworks/ddd_workshop/domain/Cart.java
@@ -2,27 +2,47 @@ package com.thoughtworks.ddd_workshop.domain;
 
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class Cart {
 
-    private ArrayList<Item> items = new ArrayList<>();
-    private ArrayList<Product> deletedProducts = new ArrayList<>();
+    private UUID uuid;
+    private List<Item> items = new ArrayList<>();
+    private List<Product> deletedProducts = new ArrayList<>();
+
+    public Cart(UUID uuid) {
+        this.uuid = uuid;
+    }
+    public Cart() {
+        this.uuid = UUID.randomUUID();
+    }
 
     public void addItem(Item item) {
         items.add(item);
     }
 
-    public ArrayList<Item> getItems() {
+    public List<Item> getItems() {
         return items;
     }
+    public UUID getUuid() { return uuid; }
 
-    public ArrayList<Product> getDeletedProducts() {
-        return deletedProducts;
-    }
+    public List<Product> getDeletedProducts() { return deletedProducts; }
 
     public void removeItem(Product product) {
         items.removeIf(item -> item.getProduct().getName().equals(product.getName()));
         addDeletedProduct(product);
+    }
+
+    public Boolean equals(Cart cart) {
+        if(cart == this) {
+            return true;
+        }
+
+        if(cart.getUuid() == this.uuid) {
+            return true;
+        }
+        return false;
     }
 
     private void addDeletedProduct(Product product) {
